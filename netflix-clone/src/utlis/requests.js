4faeaac86b62c770f465d6505e78a86db16176a6
ruta-G <ru7thane301@gmail.com>
2024-01-30 13:30:01 -0800

    axios and request

---
+++ netflix-clone/src/utlis/requests.js
@@ -0,0 +1,16 @@
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const requests = {
+    fetchTrending: `Trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOrgionals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_related?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+    fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+}
+
+export default requests;
