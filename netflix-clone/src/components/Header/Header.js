58a0154b65dd5201ebb169db7b68c4cec6fe9f7e
ruta-G <ru7thane301@gmail.com>
2024-01-30 13:48:44 -0800

    axios and request

--- netflix-clone/src/components/Header/Header.js
+++ netflix-clone/src/components/Header/Header.js
@@ -37,3 +37,11 @@ export default function Header() {
         </div>
   )
 }
+
+
+
+
+
+
+
+
--- netflix-clone/src/utlis/requests.js
+++ netflix-clone/src/utlis/requests.js
@@ -1,6 +1,4 @@
-
 const API_KEY = process.env.REACT_APP_API_KEY;
-
 const requests = {
     fetchTrending: `Trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOrgionals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
